6ad9d730ca4f684cf4b5704c4396828f7f606639
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-14 12:41:54 +0545

    add socket cores cookie

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.json({limit : "20kb"}))
+
+app.use(express.urlencoded({extended: true , limit:"16kb" }))
+
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+app.get("/" , (req,res)=>{
+    res.send("message");
+});
+
+
+
+
+
+
+export {app}
