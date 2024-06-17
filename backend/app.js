a04702208752707ebbd4b593670d55a02f12752b
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-17 14:47:54 +0545

    add cors

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin:'*',
     credentials: true
 }))
 const server = http.createServer(app);
