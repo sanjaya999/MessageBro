23926deae146995c587dfd4013a3a12384eea72c
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-18 13:04:47 +0545

    message router

---
+++ backend/routes/message.route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { createMessage , getMessage } from "../controllers/message.controller.js";
+
+
+
+
+ const mrouter = Router();
+
+
+
+
+
+
+
+mrouter.route("/createMessage").post(createMessage);
+mrouter.route("/getmessage/:chatId").get(getMessage);
+
+export default mrouter
