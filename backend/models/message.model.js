74c28250eb0cf1f2a996fefa9f01b40a01dbfbe3
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-18 13:04:15 +0545

    add message schema

---
+++ backend/models/message.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    chatId : String,
+    senderId : String,
+    text: String,
+},
+{timestamps:true
+})
+
+export const messageModel = mongoose.model("Message" , messageSchema);
