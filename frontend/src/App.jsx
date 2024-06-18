599e079ff6d5afb3b276415213bafda58319121d
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-18 13:05:27 +0545

    added frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import {Routes , Route , Navigate} from "react-router-dom"
 import Chat from "./pages/Chat.jsx"
 import Register from "./pages/register.jsx"
 import Login from "./pages/Login.jsx"
+import { useContext } from "react"
+import { AuthContext } from "./context/AuthContext.jsx"
 
 
 function App() {
   
-
+const {user} = useContext(AuthContext);
   return (
     <Routes>
-      <Route path="/" element={<Chat />}/>
-      <Route path="/register" element={<Register />}/>
-      <Route path="/login" element={<Login />}/>
+      <Route path="/" element={user ? <Chat />:<Login/>}/>
+      <Route path="/register" element={user ? <Chat />:<Register/>}/>
+      <Route path="/login" element={user ? <Chat />:<Login/>}/>
       <Route path="*" element={<Navigate to="/" />}/>
       
     </Routes>
