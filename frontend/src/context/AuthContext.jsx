27e4c9a91936ee122f02baac4bf4f9ba631bebef
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-18 13:06:00 +0545

    add login and register  context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,11 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { postReq, baseUrl } from "../utils/services.js";
-
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [registerError, setRegisterError] = useState(null);
+    const [loginError, setloginError] = useState(null);
+    const [isloginLoading, setisloginLoading] = useState(false);
+
     const [isRegisterLoading, setIsRegisterLoading] = useState(false);
     const [registerInfo, setRegisterInfo] = useState({
         userName: "",
@@ -13,18 +15,56 @@ export const AuthContextProvider = ({ children }) => {
         password: ""
     });
 
+    const [loginInfo, setloginInfo] = useState({
+        
+        email: "",
+        password: ""
+    });
+
     console.log("registration info", registerInfo);
+    console.log("login info" , loginInfo)
+
+    useEffect(()=>{
+        const user = localStorage.getItem("user");
+        setUser(JSON.parse(user))
+    },[])
 
     const updateRegisterInfo = (info) => {
         setRegisterInfo(info);
     };
+    
+    const updateloginInfo = (info) => {
+        setloginInfo(info);
+    };
 
     const registerUser = async () => {
         setIsRegisterLoading(true);
-        setRegisterError(null);
+        setloginError(null);
     
         try {
-            const response = await postReq(`${baseUrl}/register`, JSON.stringify(registerInfo));
+            const response = await postReq(`${baseUrl}/login`, JSON.stringify(loginInfo));
+            console.log(response);
+    
+            if (response.ok) {
+                setUser(response.data);
+                localStorage.setItem("user", JSON.stringify(response.data));
+            } else {
+                setloginError(response.error || "Registration failed");
+            }
+        } catch (error) {
+            console.error("Registration error:", error);
+            setloginError(error.message || "An error occurred during registration");
+        } finally {
+            setloginError(false);
+        }
+    };
+
+    const loginUser = useCallback(async()=>{
+        setisloginLoading(true);
+        setisloginLoading(null);
+    
+        try {
+            const response = await postReq(`${baseUrl}/login`, JSON.stringify(loginInfo));
             console.log(response);
     
             if (response.ok) {
@@ -39,6 +79,11 @@ export const AuthContextProvider = ({ children }) => {
         } finally {
             setIsRegisterLoading(false);
         }
+    },[loginInfo])
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem("user");
     };
 
     return (
@@ -49,7 +94,13 @@ export const AuthContextProvider = ({ children }) => {
                 registerUser,
                 registerError,
                 updateRegisterInfo,
-                isRegisterLoading
+                isRegisterLoading,
+                loginError,
+                isloginLoading,
+                loginUser,
+                updateloginInfo,
+                loginInfo,
+                logout
             }}
         >
             {children}
