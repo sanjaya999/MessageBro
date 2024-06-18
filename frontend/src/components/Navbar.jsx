43ad09c94e2a21465f14843455ee8a99e3d7c3b5
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-18 13:05:36 +0545

    navbar added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,40 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import { NavLink } from "react-router-dom";
-
+import { AuthContext } from '../context/AuthContext';
 
 function Navbar() {
-  return (
-    
-    
-    <header>
-    <nav>
-      <div className="navigation">
-        <ul className="navlink">
-          <li>
-            <NavLink to="/Home"  className ={ ({isActive})=> `${isActive? "yes-nav-text" : "no-nav-text"}`}>
-            
-              Home
-            </NavLink>
-          </li>
-         
-          <li className="start">
-              <NavLink to="/register" className="login">
-                Register
-              </NavLink>
-            </li>
-          
-            <li className="start">
-              <NavLink to="/Login" className="login">
-                Login
-              </NavLink>
-            </li>
-          
-        </ul>
-      </div>
-    </nav>
-  </header>
+  const { user, logout } = useContext(AuthContext); // Assuming you have a logout function in your AuthContext
 
-  )
+  return (
+    <header>
+      <nav>
+        <div className="navigation">
+          <ul className="navlink">
+            <li>
+              <NavLink to="/Home" className={({ isActive }) => `${isActive ? "yes-nav-text" : "no-nav-text"}`}>
+                Home
+              </NavLink>
+            </li>
+            {user ? (
+              <>
+                <span>{user.data.userName || user.data.user.userName}</span>
+                <li className="start">
+                  <button onClick={logout} className="logout">
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="start">
+                  <NavLink to="/register" className="login">
+                    Register
+                  </NavLink>
+                </li>
+                <li className="start">
+                  <NavLink to="/Login" className="login">
+                    Login
+                  </NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
