feb017498a6c0b4d01eee7f35f97f7a67e2f5477
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-19 20:47:58 +0545

    add chatcontext

---
+++ frontend/src/context/ChatContext.jsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+import { baseUrl ,getReq , postReq } from "../utils/services.js";
+
+
+export const ChatContext = createContext();
+
+export const chatContextProvider = ({children , user})=>{
+     
+}
