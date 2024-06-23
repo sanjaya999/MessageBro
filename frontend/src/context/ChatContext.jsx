656cf0bb6c9c3da389a8f6e70af649bab54e5ad7
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-23 12:13:29 +0545

    retrive chat component

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -1,9 +1,42 @@
-import { createContext } from "react";
+import { createContext, useState , useEffect} from "react";
 import { baseUrl ,getReq , postReq } from "../utils/services.js";
 
 
 export const ChatContext = createContext();
 
-export const chatContextProvider = ({children , user})=>{
+export const ChatContextProvider = ({children , user})=>{
+     const [userChats , setuserChats] = useState(null)
+     const [isuserChatLoading , setisuserChatLoading] = useState(false);
+     const [userChatError , setuserChatError] = useState(null);
      
+        useEffect(() => {
+            const getUserChats = async()=>{
+                console.log("this is user " ,user?.data.user._id);
+
+                if(user?.data.user._id){
+                    setisuserChatLoading(true)
+                    setuserChatError(null)
+                    console.log("this is user " ,user?.data.user._id);
+                    const response = await getReq(`http://localhost:9001/api/v1/chat/finduserchat/${user?.data.user._id}`)
+                    
+                    setisuserChatLoading(false)
+
+                
+                if(response.error){
+                    return setuserChatError(response.data);
+                
+            }
+                setuserChats(response.data)
+
+            }}
+            getUserChats();
+        }, [user]);
+    
+     return <ChatContext.Provider value={{userChats , 
+        userChatError,
+        isuserChatLoading
+     
+     }}>
+        {children}
+     </ChatContext.Provider>
 }
