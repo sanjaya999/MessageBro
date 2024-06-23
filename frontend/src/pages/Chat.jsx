335ad06cf20e1542374c6a85bbe6648c0731bb36
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-23 12:13:52 +0545

    use chat context

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import UserArea from './UserArea'
 import Message from './Message.jsx'
+import { ChatContext } from '../context/ChatContext.jsx'
 
 
 function Chat() {
+  const {userChats , isuserChatsLoading , userChatError} = useContext(ChatContext)
+  console.log(userChats);
   return (
     <>
       <h1>Chat</h1>
       <div className="Message">
          <Message />
+         
       </div>
 
       <div className="userarea">
