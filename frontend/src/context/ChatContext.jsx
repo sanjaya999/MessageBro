7232cec7f155bb679ed8236a8ec7ae415514165e
Sanjaya <poudelsanjaya12@gmail.com>
2024-06-25 11:26:22 +0545

    add chat context

--- frontend/src/context/ChatContext.jsx
+++ frontend/src/context/ChatContext.jsx
@@ -1,42 +1,49 @@
-import { createContext, useState , useEffect} from "react";
-import { baseUrl ,getReq , postReq } from "../utils/services.js";
-
+import { createContext, useState, useEffect } from "react";
+import { baseUrl, getReq, postReq } from "../utils/services.js";
 
 export const ChatContext = createContext();
 
-export const ChatContextProvider = ({children , user})=>{
-     const [userChats , setuserChats] = useState(null)
-     const [isuserChatLoading , setisuserChatLoading] = useState(false);
-     const [userChatError , setuserChatError] = useState(null);
-     
-        useEffect(() => {
-            const getUserChats = async()=>{
-                console.log("this is user " ,user?.data.user._id);
+export const ChatContextProvider = ({ children, user }) => {
+  const [userChats, setuserChats] = useState(null);
+  const [isuserChatLoading, setisuserChatLoading] = useState(false);
+  const [userChatError, setuserChatError] = useState(null);
+  const [thischat, setthischat] = useState(null);
 
-                if(user?.data.user._id){
-                    setisuserChatLoading(true)
-                    setuserChatError(null)
-                    console.log("this is user " ,user?.data.user._id);
-                    const response = await getReq(`http://localhost:9001/api/v1/chat/finduserchat/${user?.data.user._id}`)
-                    
-                    setisuserChatLoading(false)
+  useEffect(() => {
+    const getchats = async () => {
+      const response = await getReq(`${baseUrl}/findall`);
+      console.log("this is getchats", response);
+    };
+    getchats();
+  }, []);
 
-                
-                if(response.error){
-                    return setuserChatError(response.data);
-                
-            }
-                setuserChats(response.data)
+  useEffect(() => {
+    const getUserChats = async () => {
+      let userID = user?.data.user._id;
+      if (userID) {
+        setisuserChatLoading(true);
+        setuserChatError(null);
+        console.log("this is chatcontext id ", user?.data.user._id);
+        const response = await getReq(
+          `http://localhost:9001/api/v1/chat/finduserchat/${userID}`
+        );
+        console.log("this is response in chatcontext", response);
+        setisuserChatLoading(false);
 
-            }}
-            getUserChats();
-        }, [user]);
-    
-     return <ChatContext.Provider value={{userChats , 
-        userChatError,
-        isuserChatLoading
-     
-     }}>
-        {children}
-     </ChatContext.Provider>
-}
+        if (response.error) {
+          return setuserChatError(response.data);
+        }
+        setuserChats(response.data);
+      }
+    };
+    getUserChats();
+  }, [user]);
+
+  return (
+    <ChatContext.Provider
+      value={{ userChats, userChatError, isuserChatLoading }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
